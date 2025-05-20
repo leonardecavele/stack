861457aa8f65f4a4630ddc01245af5a5c943cf45
Gabin <123273220+Gabinap@users.noreply.github.com>
2025-05-21 00:21:05 +0200

    Update executer.h

--- include/executer.h
+++ include/executer.h
@@ -1,6 +1,32 @@
-#ifndef EXECUTER_H
-#define EXECUTER_H
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: Gabinap <gab@mail>                         #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-05-20 22:19:25 by Gabinap           #+#    #+#             */
+/*   Updated: 2025-05-20 22:19:25 by Gabinap          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef EXECUTOR_H
+# define EXECUTOR_H
 
-void execute(INSTR *instructions);
+# include "instructions.h"
+# include "stack.h"
+
+/* Fonction principale d'exécution */
+void    execute_program(INSTR *instructions, int count);
+
+/* Fonctions d'opérations */
+int     execute_push(t_stack *stack, int value);
+int     execute_pop(t_stack *stack);
+void    execute_clear(t_stack *stack);
+int     execute_add(t_stack *stack);
+int     execute_sub(t_stack *stack);
+int     execute_mul(t_stack *stack);
+int     execute_div(t_stack *stack);
+void    execute_print(t_stack *stack, int has_arg, int arg);
+void    execute_dump(t_stack *stack);
 
 #endif
